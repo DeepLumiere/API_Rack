a4f091784a84c3d6f14550250657cc11557823c9
Deep <deepensify@gmail.com>
2025-06-05 17:54:42 +0530

    Added Contacts.API

--- DeepRack/Data/ApplicationDbContext.cs
+++ DeepRack/Data/ApplicationDbContext.cs
@@ -6,5 +6,7 @@ namespace DeepRack.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     public DbSet<Appointment> Appointments { get; set; }
-    public DbSet<Notes> Notes { get; set; } // <-- This is needed
+    public DbSet<Notes> Notes { get; set; }
+    public DbSet<Contacts> Contacts { get; set; }
+    
 }
---
+++ DeepRack/Models/Contacts.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace DeepRack.Models;
+
+public class Contacts
+{
+    [Key]
+    public int Id { get; set; }
+    [Required] public string Name { get; set; }
+    public string Email { get; set; }
+    [Required] public string Phone { get; set; }
+    public string Address { get; set; }
+    public string Notes { get; set; }
+}
